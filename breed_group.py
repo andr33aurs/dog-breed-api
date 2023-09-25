4a181b209e28432129a51bf409c5c4c73a740c99
andr33aurs <dr.ursachi@gmail.com>
2023-09-25 22:34:33 +0300

    added post

--- breed_group.py
+++ breed_group.py
@@ -1,10 +1,13 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from config import db_config
 import mysql.connector
 
 
 class BreedGroupList(Resource):
-    """returns the breed groups from mySQL database(dog-breeds)"""
+    """returns the breed groups from mySQL database(dog-breeds) in JSON format"""
+
+    parser = reqparse.RequestParser()
+    parser.add_argument("name", required=True, help="Property 'name' is required!")
 
     def get(self):
         connection = mysql.connector.connect(**db_config)
@@ -18,6 +21,25 @@ class BreedGroupList(Resource):
         connection.close()
 
         return {"data": breed_groups}, 200
+
+    def post(self):
+        connection = mysql.connector.connect(**db_config)
+
+        cursor = connection.cursor(dictionary=True)
+
+        data = BreedGroupList.parser.parse_args()
+        cursor.execute("insert into breed_groups (name) values (%s)", (data["name"],))
+
+        breed_group_id = cursor.lastrowid
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        return {"data": {
+            "id": breed_group_id,
+            "name": data["name"]
+        }}, 201
 
 
 class BreedGroupItem(Resource):
@@ -35,7 +57,7 @@ class BreedGroupItem(Resource):
 
         if breed_group is None:
             connection.close()
-            return {"message": "Breed group with id '{}' not found.".format(id)}, 404
+            return {"message": f"Breed group with id '{id}' not found."}, 404
 
         breeds_cursor = connection.cursor(dictionary=True)
         breeds_cursor.execute("select * from breeds where breed_group_id=%s", (id,))
@@ -46,7 +68,3 @@ class BreedGroupItem(Resource):
         connection.close()
 
         return {"data": breed_group}, 200
-
-
-
-
